9ea42e3e5e8e6fa7a67577adaf11d8472c49768d
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-07 22:30:37 +1200

    Another example of prompting user input

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 # Print 
 # print('Hello, World!')
 
-# # Create a variable
+# Create a variable
 # first_name = "Dainy"
 # print(first_name, "is learning Python")
 
 # Create an input from a user
-first_name = input('What is your name? ')
-print('Hello,', first_name)
-print(first_name, 'is learning Python!')
+# first_name = input('What is your name? ')
+# print('Hello,', first_name)
+# print(first_name, 'is learning Python!')
+
+# Prompt the user
+favorite_color = input("What is your favorite color? ")
