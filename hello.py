1f0a2f49ca092fb78b70835a1823cce085f027f3
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-12 22:54:58 +1200

    Added if elif and else

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ first_name = input("What is your first name? ")
 print("Hello,", first_name)
 if first_name == "Dainy":
     print(first_name, "is learning Python")
+elif first_name == "Faye":
+    print(first_name, "is learning with fellow students")
+else:
+    print("You should totally learn Python, {}".format(first_name))
 print("Have a great day {}!".format(first_name))
