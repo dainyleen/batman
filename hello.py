01d494993f69af5014a32599fe02b0e3715847e3
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-12 23:07:31 +1200

    Added # for making comments in Python

--- hello.py
+++ hello.py
@@ -5,5 +5,10 @@ if first_name == "Dainy":
 elif first_name == "Faye":
     print(first_name, "is learning with fellow students")
 else:
+    # This is just in case we have a younger user 
+    age = int(input("How old are you? "))
+    if age <= 6:
+        print("Wow you're {}! If you're confident with your reading already...".format(age))
+
     print("You should totally learn Python, {}".format(first_name))
 print("Have a great day {}!".format(first_name))
