48aeffcf9ea7c6f70c61ff40e0979af2b0dfc84c
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 15:02:52 +1200

    Declared variables

---
+++ masterticket.py
@@ -0,0 +1,8 @@
+# Declare variables
+TICKET_PRICE = 10
+tickets_remaining = 100
+
+# Output how many tickets are remaining using tickets_remaining variable
+print("There are {} tickets remaining.".format(tickets_remaining))
+
+# Gather the user's name and assign it to a new variable
