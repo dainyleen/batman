9c3cfdce4a96b088f31342ce0d7840e526fde143
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 15:52:46 +1200

    Added more Python comments

--- masterticket.py
+++ masterticket.py
@@ -6,3 +6,8 @@ tickets_remaining = 100
 print("There are {} tickets remaining.".format(tickets_remaining))
 
 # Gather the user's name and assign it to a new variable
+name = input("What is your name? ")
+
+# Prompt the user by name and ask how many tickets they would like
+
+# Calculate the price (number of tickers multiplied by the price)
