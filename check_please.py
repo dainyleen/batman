a2ae0ff568f15af94760a7f24cbb8f5ddfce1e2a
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 13:34:12 +1200

    Created a simple math calculation using Python

--- check_please.py
+++ check_please.py
@@ -1,8 +1,12 @@
 import math
 
 def split_check(total, number_of_people):
-    cost_per_person = total / number_of_people
+    cost_per_person = math.ceil(total / number_of_people)
     return cost_per_person
 
-amount_due = split_check(104.32, 5)
+total_due = float(input("What is the total? "))
+number_of_people = int(input("How many people? "))
+amount_due = split_check(total_due, number_of_people)
+
+
 print("Each person owes ${}".format(amount_due))
