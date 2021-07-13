e4bfb5e70ea9d82dbd879a8283997d20a7933623
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 13:30:32 +1200

    Initialized import math

--- check_please.py
+++ check_please.py
@@ -1,3 +1,8 @@
+import math
+
 def split_check(total, number_of_people):
     cost_per_person = total / number_of_people
     return cost_per_person
+
+amount_due = split_check(104.32, 5)
+print("Each person owes ${}".format(amount_due))
