da7b2003087902867429d5979dc22fe96b36dc50
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 15:57:19 +1200

    Prompted the user to enter number of tickets and output the result

--- masterticket.py
+++ masterticket.py
@@ -9,5 +9,11 @@ print("There are {} tickets remaining.".format(tickets_remaining))
 name = input("What is your name? ")
 
 # Prompt the user by name and ask how many tickets they would like
+num_tickets = input("How many tickets would you like, {}? ".format(name) )
 
-# Calculate the price (number of tickers multiplied by the price)
+# Calculate the price (number of tickers multiplied by the price) and assign it to a variable
+total_price = int(float(num_tickets * TICKET_PRICE))
+
+# Output the price to the screen
+print(total_price)
+
