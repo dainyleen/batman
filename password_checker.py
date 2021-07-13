271c77e1f36437536645b8c8a175dc5bdc648db7
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 14:36:50 +1200

    Added an if statement to make sure the user has only 3 attempts

--- password_checker.py
+++ password_checker.py
@@ -1,4 +1,10 @@
+import sys
+
 password = input("Please enter a password: ")
+attempt_count = 1
 while password != 'cinderella':
+    if attempt_count > 3:
+        sys.exit("Too many invalid passowrd attempts")
     password = input("Invalid password, try again: ")
+    attempt_count += 1
 print("Welcome, Cinderella!")
